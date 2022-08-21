2f85c336c98e566dcfe14e1d5bae6bd0c10a4c9a
facundobarquet <110636222+facundobarquet@users.noreply.github.com>
2022-08-20 23:58:00 -0300

    login final

--- js/init.js
+++ js/init.js
@@ -44,7 +44,7 @@ let myStorage=window.localStorage;
 // Localstorage con DOM, para el inicio de sesion
 let actualPage=window.location.pathname.split("/").slice(-1);
 document.addEventListener("DOMContentLoaded",function(){
-  if (!myStorage.getItem("logged") && actualPage=="login.html"){
+  if (!myStorage.getItem("logged") && actualPage!="login.html"){
     window.location.href="login.html";
   }
 })
